dea7b197a4536fc6cd8799aa1afd40bbf2c32f94
NiangPro <NiangProgrammeur@gmail.com>
2026-02-24 11:01:28 +0000

    bien

---
+++ database/migrations/2026_02_24_104000_increase_cover_image_length_in_job_articles_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * Les URLs externes (ex. Blogger) peuvent dépasser 255 caractères.
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE job_articles MODIFY cover_image VARCHAR(2048) NULL');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('ALTER TABLE job_articles MODIFY cover_image VARCHAR(255) NULL');
+    }
+};
